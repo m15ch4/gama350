67a9e664e7e7b4a1abd22ff5e38a87fca901561e
Administrator <admin@example.com>
2025-06-02 22:53:52 +0200

    absolute logging path

--- logger/logger.go
+++ logger/logger.go
@@ -1,17 +1,17 @@
 package logger
 
 import (
-    "io"
-    "log"
-    "os"
+	"io"
+	"log"
+	"os"
 )
 
 var Logger *log.Logger
 
 func InitLogger() {
-    file, err := os.OpenFile("app.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
-    if err != nil {
-        log.Fatalf("Could not open log file: %v", err)
-    }
-    Logger = log.New(io.MultiWriter(os.Stdout, file), "", log.LstdFlags)
+	file, err := os.OpenFile("/var/log/gama/app.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatalf("Could not open log file: %v", err)
+	}
+	Logger = log.New(io.MultiWriter(os.Stdout, file), "", log.LstdFlags)
 }
